9df18a7baeecf67ba1df2ee178cf6b0e74dfd7f0
Benjamin Coullie <ben.coullie@vendhq.com>
2018-01-22 17:08:11 +1300

    starting to add chart js (added additional fields and wait to load)

--- src/components/gameTab/gameTab.js
+++ src/components/gameTab/gameTab.js
@@ -9,18 +9,30 @@ class GameTab extends Component {
   }
 
   componentDidMount () {
-    fetch('http://ow-api.herokuapp.com/profile/pc/us/VoA-1535').then(response => {
-      response.json().then(data => {
-        this.setState({ profile: data })
+    fetch('http://ow-api.herokuapp.com/profile/pc/us/VoA-1535')
+      .then(response => {
+        response.json().then(data => {
+          this.setState({ profile: data })
+        })
       })
-    })
+      .then(() => {
+        console.log('Profile info:', this.state.profile)
+      })
   }
 
   render () {
+    const ready = this.state && this.state.profile
+
     return (
       <div className='gameTab-wrapper'>
         <div>
-          <h1>Hello world!</h1>
+          <h1>{ready && this.state.profile.username}</h1>
+          <h1>Season stats:</h1>
+          <h1>Time in Comp: {ready && this.state.profile.playtime.competitive}</h1>
+          <h1>Time in Quickplay: {ready && this.state.profile.playtime.quickplay}</h1>
+          <canvas id='myChart' />
+          <h1>Rank:</h1>
+          <img src={ready && this.state.profile.competitive.rank_img} />
         </div>
       </div>
     )
